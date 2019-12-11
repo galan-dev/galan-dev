455345ec6c3ee27b7fae5071595960ea44e0c89b
juddorcasitas <orcasitas.jr@gmail.com>
2019-12-11 09:25:51 -0500

    conditional stop/start sh

--- codedeploy/app_start.sh
+++ codedeploy/app_start.sh
@@ -3,5 +3,11 @@
 # This script is used to start the application
 cd /usr/galan-dev
 service nginx start
-npm start &
+if pgrep -x "node" > /dev/null
+then
+    echo "App Start: Node is already running to cont.."
+else
+    echo "App Start: Node Not Running so Start"
+    npm start &
+fi
 # pm2 start /usr/galan-dev/bin/www -n www -f
--- codedeploy/app_stop.sh
+++ codedeploy/app_stop.sh
@@ -3,5 +3,13 @@
 # This script is used to stop application
 # cd /usr/galan-dev
 service nginx stop
-killall -9 node
+
+if pgrep -x "node" > /dev/null
+then
+    echo "App Stop: Node is Running so Stop"
+    killall -9 node
+else
+    echo "App Stop: No Node Process Running so cont.."
+fi
+
 # pm2 stop www || true
