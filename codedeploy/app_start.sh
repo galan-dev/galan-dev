5c63af4b505e4be876343bb2817c23d278eb4159
juddorcasitas <orcasitas.jr@gmail.com>
2019-12-11 10:08:17 -0500

    fix broken sh scripts

--- codedeploy/app_start.sh
+++ codedeploy/app_start.sh
@@ -7,9 +7,9 @@ if pgrep -x "node" > /dev/null
 then
     echo "App Start: Node is already running so stop and run new.."
     killall -9 node
-    npm start &
+    npm start && exit 0
 else
-    echo "App Start: Node Not Running so Start"
-    npm start &
+    echo "App Start: Node Not Running so Start" 
+    npm start && exit 0
 fi
 # pm2 start /usr/galan-dev/bin/www -n www -f
